ae5a836edf757b9a4e8484a9b46ef75a419345db
Raphael LEBER <raphael.leber@gmail.com>
2018-12-11 14:26:58 +0100

    fix line missing in shortAngleDiff

--- script/localPlanner_student.py
+++ script/localPlanner_student.py
@@ -169,6 +169,8 @@ class LocalPlanner:
         """
             Returns the shortest angle between 2 angles in the trigonometric circle
         """        
+        anglediff = fmod( (th1 - th2), 2*pi)
+
         if anglediff < 0.0:
             if fabs(anglediff) > (2*pi + anglediff) :
                 anglediff = 2*pi + anglediff
